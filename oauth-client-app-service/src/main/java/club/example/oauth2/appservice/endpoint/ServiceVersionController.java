aacf7963c167ef1a80983395c4b2209536ae7e22
Cagliostro <linframe@outlook.com>
2020-03-15 17:18:48 +0800

    微服务之间授权

--- oauth-client-app-service/src/main/java/club/example/oauth2/appservice/endpoint/ServiceVersionController.java
+++ oauth-client-app-service/src/main/java/club/example/oauth2/appservice/endpoint/ServiceVersionController.java
@@ -2,6 +2,8 @@ package club.example.oauth2.appservice.endpoint;
 
 import club.example.oauth2.appservice.entity.ServiceVersion;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServiceVersionController {
 
     @GetMapping
-    public ServiceVersion getVersion() {
+    public ServiceVersion getVersion(@AuthenticationPrincipal OAuth2Authentication auth2Authentication) {
+        log.info("getVersion auth2Authentication  = {}", auth2Authentication.getUserAuthentication().getPrincipal());
         ServiceVersion serviceVersion = new ServiceVersion();
         serviceVersion.setName("VersionNamed");
         return serviceVersion;
